d79ce13e43d15c0aea6aeb61df1b4346333f7cb0
0vercl0k <0vercl0k@tuxfamily.org>
2019-06-19 19:59:59 -0700

    Tweak the pelicanconf file to get rid of some warnings during generation.

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u"Axel '0vercl0k' Souchet"
 SITENAME = u'Diary of a reverse-engineer'
-SITEURL = ''
+SITEURL = 'https://doar-e.github.io'
 
 PATH = 'content'
 
@@ -16,8 +16,8 @@ DEFAULT_LANG = u'English'
 FEED_ATOM = 'feeds/atom.xml'
 FEED_RSS = 'feeds/rss.xml'
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/category.%s.atom.xml'
-AUTHOR_FEED_ATOM = 'feeds/author.%s.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/category.{slug}.atom.xml'
+AUTHOR_FEED_ATOM = 'feeds/author.{slug}.atom.xml'
 
 STATIC_PATHS = ['downloads', 'images', 'presentations']
 ARTICLE_PATHS = ['articles']
@@ -58,4 +58,4 @@ ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
 ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
