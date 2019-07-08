af9dad93e7446f227c7b654e8d9b1ea5a4c12bc7
0vercl0k <0vercl0k@tuxfamily.org>
2019-07-08 08:47:24 -0700

    Emmergency procedure!

--- update_remote.bat
+++ update_remote.bat
@@ -1,1 +1,11 @@
 git subtree push --prefix output origin master
+
+REM In case of emmergency:
+REM # Ensure we are in src
+REM git checkout src
+REM # Creates chaos branch
+REM git subtree split --prefix output -b chaos
+REM # Push chaos onto origin/master
+REM git push --force origin chaos:master
+REM # Deletes chaos
+REM git branch -D chaos
